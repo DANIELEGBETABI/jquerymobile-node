472500ea606733175aa8052da26bab53ecaf931b
Tommy MacWilliam <tmacwilliam@cs.harvard.edu>
2012-01-17 00:23:13 -0500

    first implementation of chat server

--- node/app.js
+++ node/app.js
@@ -1,35 +1,38 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-
+var express = require('express');
 var app = module.exports = express.createServer();
+var io = require('sockiet.io').listen(app);
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure('development', function() {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('production', function() {
+	app.use(express.errorHandler()); 
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+// create socket namespace for the chat room
+var chat = io.of('/chat').on('connection', function(socket) {
+	// new client has joined the chat
+	socket.on('join', function(name) {
+		// associate name with this socket
+		socket.set('name', name, function() {
+			// inform client we're ready
+			socket.emit('ready');
+		});
+	});
+
+	// client has sent a new change message
+	socket.on('message', function(message) {
+		// get name associated with this socket
+		socket.get('name', function(error, name) {
+			// send message to all chat participants
+			socket.broadcast.emit('message', { 
+				from: name,
+				message: message 
+			});
+		});
+	});
 });
-
-// Routes
-
-app.get('/', routes.index);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
